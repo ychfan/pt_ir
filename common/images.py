7c86ae9ad1ed22be5a85083abb5e3a2ba01be2c1
Yuchen Fan <fyc0624@gmail.com>
2020-06-08 03:03:45 -0500

    add matlab_imresize submodule

--- common/images.py
+++ common/images.py
@@ -1,1 +1,8 @@
-from third_party.matlab_imresize.imresize import imresize
+import numpy as np
+from third_party.matlab_imresize.imresize import imresize as matlab_imresize
+
+
+def imresize(I, scalar_scale=None, output_shape=None):
+  I = matlab_imresize(I.astype(np.float64), scalar_scale, output_shape)
+  I = np.around(np.clip(I, 0, 255)).astype(np.uint8)
+  return I
